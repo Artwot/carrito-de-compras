9fa1338bbf36ee1249a6eeedcc6f942dc1e25909
Artwot <jsarabiar98@gmail.com>
2021-11-05 16:21:40 -0600

    Se agregaron estilos a los componentes y el botón

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Productos from "./components/Productos";
+import Layout from "./components/Layout";
+import Title from "./components/Title";
+import Navbar from "./components/Navbar";
+class App extends Component {
+  state = {
+    productos: [
+      { name: "Tomate", price: 1500, img: "/productos/tomate.jpg" },
+      { name: "Chícharos", price: 2500, img: "/productos/arbejas.jpg" },
+      { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
+    ],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Layout>
+          <Title />
+          <Productos //Primer componente
+            agregarAlCarro={() => console.log("No hace nada")}
+            productos={this.state.productos}
+          />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
